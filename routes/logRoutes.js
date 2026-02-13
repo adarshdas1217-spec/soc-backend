6937cf8f31b486a57591fbf7ba4a246a5721a696
adarshdas1217-spec <adarshdas1217@gmail.com>
2026-02-13 09:38:50 +0530

    some deployment changes

--- routes/logRoutes.js
+++ routes/logRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { createLog } = require("../controllers/logController");
-const authMiddleware = require("../middleware/authMiddleware");
+
 
 router.post("/", createLog);
 
